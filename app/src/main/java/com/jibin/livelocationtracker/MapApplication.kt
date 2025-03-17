48fba4d577516b943a53caf1f32c6a7a6d79a0db
Jibin Joseph <jibinjoseph.dev22@gmail.com>
2025-03-17 02:45:16 -0600

    feat(map): Hilt App module setup with LocationRepository , FusedLocationProvider

---
+++ app/src/main/java/com/jibin/livelocationtracker/MapApplication.kt
@@ -0,0 +1,8 @@
+package com.jibin.livelocationtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MapApplication : Application() {
+}
---
+++ app/src/main/java/com/jibin/livelocationtracker/di/AppModule.kt
@@ -0,0 +1,40 @@
+package com.jibin.livelocationtracker.di
+
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.jibin.livelocationtracker.data.repository.LocationRepositoryImpl
+import com.jibin.livelocationtracker.domain.repository.LocationRepository
+import com.jibin.livelocationtracker.domain.usecases.GetAddressUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent :: class)
+object AppModule {
+
+    @Provides
+    fun provideFusedLocationProviderClient(
+        @ApplicationContext context: Context
+    ): FusedLocationProviderClient {
+        return LocationServices.getFusedLocationProviderClient(context)
+    }
+
+    @Provides
+    fun provideLocationRepository(
+        fusedLocationProviderClient: FusedLocationProviderClient,
+        @ApplicationContext context: Context
+    ): LocationRepository {
+        return LocationRepositoryImpl(fusedLocationProviderClient, context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetAddressUseCase(repository: LocationRepository): GetAddressUseCase {
+        return GetAddressUseCase(repository)
+    }
+}
